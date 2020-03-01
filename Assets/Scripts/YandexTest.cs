646363061142daefe50372ce097f8c51d4f8d23e
aligungr <aligng1620@gmail.com>
2020-03-01 06:04:17 +0300

    YandexTranslateText implemented
    Co-authored-by: aligungr <aligng1620@gmail.com>
    Co-authored-by: beyzaaltuntas <baltuntas@etu.edu.tr>

---
+++ Assets/Scripts/YandexTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YandexTest : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine("Loop");
+    }
+
+    private IEnumerator Loop()
+    {
+        var translate = new YTranslate("trnsl.1.1.20200229T212133Z.e184ed33afc29e8a.334aa1670617dbfd188616640084847d586e1852");
+
+        var sw = new Stopwatch();
+        while (true)
+        {
+            YTranslate.Result result = null;
+            Action<YTranslate.Result> action = (r) => {
+                result = r;
+            };
+            sw.Restart();
+
+            yield return translate.translate("hello" + UnityEngine.Random.value, YTranslate.Language.TR, action);
+            if (result == null)
+                continue;
+            sw.Stop();
+            GetComponentInChildren<Text>().text = sw.ElapsedMilliseconds + " ms";
+
+            yield return new WaitForSecondsRealtime(1.0f);
+        }
+    }
+}
