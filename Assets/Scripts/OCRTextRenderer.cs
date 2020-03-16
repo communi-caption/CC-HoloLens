5fe7e67bb68e7c1be595a5c98b4cb16808ae8da4
beyzaaltuntas <baltuntas@etu.edu.tr>
2020-03-17 01:58:57 +0300

    Rotation added.
    Co-authored-by: beyzaaltuntas <baltuntas@etu.edu.tr>
    Co-authored-by: aligungr <aligng1620@gmail.com>

--- Assets/Scripts/OCRTextRenderer.cs
+++ Assets/Scripts/OCRTextRenderer.cs
@@ -5,55 +5,49 @@ using UnityEngine.UI;
 
 public class OCRTextRenderer : MonoBehaviour
 {
-    Text sentence;
-    Image rect;
-
-    public RectTransform CanvasRect;
-    private RectTransform UI_Element;
+    public RectTransform canvasRect;
+    private RectTransform uiRect;
 
     void Awake()
     {
-        UI_Element = GetComponent<RectTransform>();
+        uiRect = GetComponent<RectTransform>();
     }
 
-    void Start()
-    {
-        Invoke("deneme", 2);
-    }
+    //void Start()
+    //{
+    //    Invoke("deneme", 2);
+    //}
+    //
+    //void deneme()
+    //{
+    //    Render(new Vector2(-5.3f, 0), 2, 3, 90, "asddsa");
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void deneme()
-    {
-        Render(new Vector2(-5.3f, 0),  2, 3, "asddsa");
-    }
-
-    public void Render(Vector2 center, float width, float height, string text)
+    public void Render(Vector2 center, float width, float height, float rotationZ, string text)
     {
         Vector2 centerTransformed = Camera.main.WorldToViewportPoint(center);
         Vector2 leftTransformed = Camera.main.WorldToViewportPoint(center - Vector2.left * width);
         Vector2 topTransformed = Camera.main.WorldToViewportPoint(center + Vector2.up * height);
 
-        Vector2 WorldObject_ScreenPosition = new Vector2(
-        ((centerTransformed.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
-        ((centerTransformed.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+        Vector2 centerNormalized = new Vector2(
+        ((centerTransformed.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
+        ((centerTransformed.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f)));
 
-        Vector2 WorldObject_ScreenPosition2 = new Vector2(
-        ((leftTransformed.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
-        ((leftTransformed.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+        Vector2 leftNormalized = new Vector2(
+        ((leftTransformed.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
+        ((leftTransformed.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f)));
 
-        Vector2 WorldObject_ScreenPosition3 = new Vector2(
-        ((topTransformed.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f)),
-        ((topTransformed.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f)));
+        Vector2 topNormalized = new Vector2(
+        ((topTransformed.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
+        ((topTransformed.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f)));
 
-        float widthTransformed = (WorldObject_ScreenPosition2 - WorldObject_ScreenPosition).magnitude;
-        float heightTransformed = (WorldObject_ScreenPosition3 - WorldObject_ScreenPosition).magnitude;
+        float widthTransformed = (leftNormalized - centerNormalized).magnitude;
+        float heightTransformed = (topNormalized - centerNormalized).magnitude;
 
-        UI_Element.anchoredPosition = WorldObject_ScreenPosition;
-        UI_Element.sizeDelta = new Vector2(widthTransformed, heightTransformed);
+        uiRect.anchoredPosition = centerNormalized;
+        uiRect.sizeDelta = new Vector2(widthTransformed, heightTransformed);
+        uiRect.Rotate(0, 0, rotationZ);
+
+        GetComponentInChildren<Text>().text = text;
     }
 }
